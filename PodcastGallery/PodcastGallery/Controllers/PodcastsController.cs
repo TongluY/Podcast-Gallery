ea7366308ec16736cfb4db0f467ab368c1abe1fd
Tonlu Yang <tyang328@wisc.edu>
2022-06-10 10:17:27 -0500

    Added Detail action method

--- PodcastGallery/PodcastGallery/Controllers/PodcastsController.cs
+++ PodcastGallery/PodcastGallery/Controllers/PodcastsController.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PodcastGallery.Controllers
 {
     public class PodcastsController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the podcasts controller!";
+        }
     }
 }
