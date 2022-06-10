bc719ca9706f03ece787ae44b155151bfe300628
Tonlu Yang <tyang328@wisc.edu>
2022-06-10 11:18:12 -0500

    Added Podcast Detail view

--- PodcastGallery/PodcastGallery/Controllers/PodcastsController.cs
+++ PodcastGallery/PodcastGallery/Controllers/PodcastsController.cs
@@ -10,11 +10,7 @@ namespace PodcastGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the podcasts controller!");
+            return View();
         }
     }
 }
